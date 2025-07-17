f2e29137b633a18f14c59dc8646a77ef01fcde2d
lucas.severino <lucas.severino@NT-TIOP-CPU1835>
2025-07-17 18:55:18 -0300

    configuranado o tema lightTheme ou darkTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { userAuth } from "./hooks/auth";
+import { useTheme } from "./hooks/theme";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./themes/ligthTheme";
+import { darkTheme } from "./themes/darkTheme";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const {handleAutenticateUser} = userAuth()
+    const {handleInitTheme, theme} = useTheme()
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        // Authenticate user using token saved in local storege
+         handleAutenticateUser()
+
+        // Appli theme saved by user to local storage
+        handleInitTheme()
+
+    }, []) 
+
+    return (
+        <ThemeProvider theme ={theme == 'light' ? lightTheme: darkTheme}>
+            ...
+        </ThemeProvider>
+    )
 }
 
-export default App
+export default App;
